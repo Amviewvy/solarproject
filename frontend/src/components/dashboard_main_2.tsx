226437df64005ddeb54c12a80cc678151e5cc814
ppxndd <natthapongp65@nu.ac.th>
2025-10-17 02:33:05 +0700

    make for prod

--- frontend/src/components/dashboard_main_2.tsx
+++ frontend/src/components/dashboard_main_2.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "../styles/dashboard_main_2.module.css";
 import DateRangePicker from "./Calendar_DateRangePicker";
 import TrendCard from "./TrendCard";
